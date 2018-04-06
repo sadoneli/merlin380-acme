a04f1c4666a7bf585773f9f8a9c4898f6b82dd24
sadoneli <sadoneli@gmail.com>
2018-04-06 17:33:33 +0800

    update title

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2018-04-06_16:12:15", 
     "description": "自动部署SSL证书", 
     "home_url": "Module_acme.asp", 
-    "md5": "8f21c05c035568f5e5f4fa46b938a37d", 
+    "md5": "d8f35f05eacade50463051f6e18afa80", 
     "module": "acme", 
-    "title": "Let\\'s Encrypt", 
-    "version": "1.1"
+    "title": "Lets Encrypt", 
+    "version": "1.2"
 }
