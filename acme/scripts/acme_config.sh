fc9cad99d18fa598cfb27fc465454dc850b68ba5
sadoneli <sadoneli@gmail.com>
2018-04-06 23:25:01 +0800

    update

--- acme/scripts/acme_config.sh
+++ acme/scripts/acme_config.sh
@@ -2,6 +2,7 @@
 source /koolshare/scripts/base.sh
 eval `dbus export acme`
 acme_root="/koolshare/acme"
+LOGFILE=acme_run.log
 alias echo_date='echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】:'
 mkdir -p /jffs/ssl
 
@@ -68,24 +69,24 @@ check_cert(){
 }
 
 apply_now(){
-	echo_date 开始为$acme_domain申请证书！ >> /tmp/acme_run.log
-	echo_date 证书申请过程可能会持续3分钟，请不要关闭或刷新本网页！ >> /tmp/acme_run.log
+	echo_date 开始为$acme_domain申请证书！ >> $LOGFILE
+	echo_date 证书申请过程可能会持续3分钟，请不要关闭或刷新本网页！ >> $LOGFILE
 	sleep 2
-	start_issue >> /tmp/acme_run.log
+	start_issue >> $LOGFILE
 	if [ "$?" != "0" ];then
-		echo_date 证书申请失败，请检查插件配置、域名等是否正确！！ >> /tmp/acme_run.log
-		echo_date 清理相关残留并关闭插件！！ >> /tmp/acme_run.log
+		echo_date 证书申请失败，请检查插件配置、域名等是否正确！！ >> $LOGFILE
+		echo_date 清理相关残留并关闭插件！！ >> $LOGFILE
 		rm -rf "$acme_domain" > /dev/null 2>&1
 		rm -rf http.header > /dev/null 2>&1
 		dbus set acme_enable="0"
 	else
-		echo_date 证书申请成功！>> /tmp/acme_run.log
+		echo_date 证书申请成功！>> $LOGFILE
 		echo_date 添加证书更新定时任务！
-		add_cron >> /tmp/acme_run.log
-		echo_date 安装证书！ >> /tmp/acme_run.log
-		echo_date 安装证书会重启路由器web服务，安装完成后需要重新登录路由器 >> /tmp/acme_run.log
-		echo_date 安装中，，，请等待页面自动刷新！ >> /tmp/acme_run.log
-		echo XU6J03M7 >> /tmp/acme_run.log
+		add_cron >> $LOGFILE
+		echo_date 安装证书！ >> $LOGFILE
+		echo_date 安装证书会重启路由器web服务，安装完成后需要重新登录路由器 >> $LOGFILE
+		echo_date 安装中，，，请等待页面自动刷新！ >> $LOGFILE
+		echo XU6J03M7 >> $LOGFILE
 		install_cert
 	fi
 }
@@ -118,8 +119,8 @@ start)
 	fi
 	;;
 *)
-	echo "------------------------------ Let's Encrypt merlin addon by sadog -------------------------------" > /tmp/acme_run.log
-	echo "" >> /tmp/acme_run.log
+	echo "------------------------------ Let's Encrypt merlin addon by sadog -------------------------------" > $LOGFILE
+	echo "" >> $LOGFILE
 	[ ! -L "/koolshare/init.d/S99acme.sh" ] && ln -sf /koolshare/scripts/acme_config.sh /koolshare/init.d/S99acme.sh
 	if [ "$acme_action" == "1" ];then
 	#提交按钮
@@ -135,34 +136,34 @@ start)
 					check_md5
 					if [ "$md5sum_cer_jffs"x = "$md5sum_cer_acme"x ] && [ "$md5sum_key_jffs"x = "$md5sum_key_acme"x ];then
 						#安装了，检测定时任务
-						echo_date 检测到已经为【$acme_subdomain.$acme_domain，$acme_domain】申请了证书并且正确安装，跳过！>> /tmp/acme_run.log
+						echo_date 检测到已经为【$acme_subdomain.$acme_domain，$acme_domain】申请了证书并且正确安装，跳过！>> $LOGFILE
 						cronjob=`cru l | grep acme_renew`
 						if [ "$cronjob" ];then
 							#有定时任务
-							echo_date 检测到【$acme_subdomain.$acme_domain，$acme_domain】证书自动更新定时任务正常，跳过！>> /tmp/acme_run.log
+							echo_date 检测到【$acme_subdomain.$acme_domain，$acme_domain】证书自动更新定时任务正常，跳过！>> $LOGFILE
 						else
 							#无定时任务
-							echo_date 检测到【$acme_subdomain.$acme_domain，$acme_domain】证书自动更新定时任务未添加！>> /tmp/acme_run.log
-							echo_date 添加证书更新定时任务！
-							add_cron >> /tmp/acme_run.log
+							echo_date 检测到【$acme_subdomain.$acme_domain，$acme_domain】证书自动更新定时任务未添加！>> $LOGFILE
+							echo_date 添加证书更新定时任务！！>> $LOGFILE
+							add_cron >> $LOGFILE
 						fi
 					else
 						#申请过了，但是没有安装
-						echo_date 检测到你之前生成【$acme_subdomain.$acme_domain，$acme_domain】的证书，本次跳过申请，直接安装！ >> /tmp/acme_run.log
-						echo_date 如果该证书已经过期，请本次提交完成后手动更新证书。 >> /tmp/acme_run.log
-						echo_date 添加证书更新定时任务！
-						add_cron >> /tmp/acme_run.log
-						echo_date 安装证书！ >> /tmp/acme_run.log
-						echo_date 安装证书会重启路由器web服务，安装完成后需要重新登录路由器 >> /tmp/acme_run.log
-						echo_date 安装中，，，请等待页面自动刷新！ >> /tmp/acme_run.log
-						echo XU6J03M7 >> /tmp/acme_run.log
+						echo_date 检测到你之前生成【$acme_subdomain.$acme_domain，$acme_domain】的证书，本次跳过申请，直接安装！ >> $LOGFILE
+						echo_date 如果该证书已经过期，请本次提交完成后手动更新证书。 >> $LOGFILE
+						echo_date 添加证书更新定时任务！！>> $LOGFILE
+						add_cron >> $LOGFILE
+						echo_date 安装证书！ >> $LOGFILE
+						echo_date 安装证书会重启路由器web服务，安装完成后需要重新登录路由器 >> $LOGFILE
+						echo_date 安装中，，，请等待页面自动刷新！ >> $LOGFILE
+						echo XU6J03M7 >> $LOGFILE
 						install_cert
 					fi
 				else
 					# 对应你个二级域名没申请过
 					# 删除主域名文件夹并申请更新的
-					echo_date 检测到你之前生成过该域名【$SUB.$acme_domain，$acme_domain】证书，但是本次申请的二级域名不同！ >> /tmp/acme_run.log
-					echo_date 删除原先生成的证书，重新申请新的证书！ >> /tmp/acme_run.log
+					echo_date 检测到你之前生成过该域名【$SUB.$acme_domain，$acme_domain】证书，但是本次申请的二级域名不同！ >> $LOGFILE
+					echo_date 删除原先生成的证书，重新申请新的证书！ >> $LOGFILE
 					rm -rf "$acme_domain"
 					apply_now
 				fi
@@ -175,31 +176,31 @@ start)
 			if [ -d "$acme_domain" ] && [ -f "$acme_domain/$acme_domain.key" ] && [ -f "$acme_domain/fullchain.cer" ];then
 				check_md5
 				if [ "$md5sum_cer_jffs"x = "$md5sum_cer_acme"x ] && [ "$md5sum_key_jffs"x = "$md5sum_key_acme"x ];then
-					echo_date 检测到你已经成功申请并安装证书，关闭插件仅仅会关闭证书的自动更新。>> /tmp/acme_run.log
-					echo_date 关闭插件状态下仍然可以使用手动更新对证书进行更新，请注意你的证书的过期时间！>> /tmp/acme_run.log
+					echo_date 检测到你已经成功申请并安装证书，关闭插件仅仅会关闭证书的自动更新。>> $LOGFILE
+					echo_date 关闭插件状态下仍然可以使用手动更新对证书进行更新，请注意你的证书的过期时间！>> $LOGFILE
 				else
-					echo_date 关闭插件 >> /tmp/acme_run.log
+					echo_date 关闭插件 >> $LOGFILE
 				fi
 			else
-				echo_date 关闭插件 >> /tmp/acme_run.log
+				echo_date 关闭插件 >> $LOGFILE
 			fi
 		fi
 	elif [ "$acme_action" == "2" ];then
 		#强制更新
-		echo_date 强制更新证书，即使证书未过期，请注意使用频率。>> /tmp/acme_run.log
-		force_renew >> /tmp/acme_run.log
+		echo_date 强制更新证书，即使证书未过期，请注意使用频率。>> $LOGFILE
+		force_renew >> $LOGFILE
 	elif [ "$acme_action" == "3" ];then
 		#删除证书
-		echo_date 删除所有本插件生成的证书...>> /tmp/acme_run.log
-		echo_date 路由器上已经安装的证书...>> /tmp/acme_run.log
+		echo_date 删除所有本插件生成的证书...>> $LOGFILE
+		echo_date 路由器上已经安装的证书...>> $LOGFILE
 		del_all_cert
-		echo_date 定时更新任务...>> /tmp/acme_run.log
+		echo_date 定时更新任务...>> $LOGFILE
 		del_cron
-		echo_date 关闭本插件！>> /tmp/acme_run.log
+		echo_date 关闭本插件！>> $LOGFILE
 		dbus set acme_enbale="0"
 	fi
-	echo "" >> /tmp/acme_run.log
-	echo XU6J03M6 >> /tmp/acme_run.log
+	echo "" >> $LOGFILE
+	echo XU6J03M6 >> $LOGFILE
 	dbus remove acme_action
 	;;
 esac
--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2018-04-06_16:12:15", 
     "description": "自动部署SSL证书", 
     "home_url": "Module_acme.asp", 
-    "md5": "c7fc801f4b91835eede57c9a5f40d52c", 
+    "md5": "b79439f0d8e5bf3cd480526d84df3811", 
     "module": "acme", 
     "title": "Lets Encrypt", 
-    "version": "1.5"
+    "version": "1.6"
 }
